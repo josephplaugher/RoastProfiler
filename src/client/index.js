44d1f480e8960334ae7604de0a6730fed8de840e
Joseph <josephplaugher@gmail.com>
2021-03-06 22:16:46 -0800

    the chart seems to be working. I'll ping the arduino now

--- src/client/index.js
+++ src/client/index.js
@@ -1,53 +1,48 @@
 (function () {
     const socket = io();
 
-    plot = document.getElementById('plot');
-    var layout = {
-        xaxis: {
-            title: {
-                text: "Time",
-                standoff: 40
-            },
-            range: [0, 12],
-            autotick: false,
-            ticks: 'outside',
-            tick0: 0,
-            dtick: 0.5,
-            ticklen: 5,
-            tickwidth: 5,
-            tickcolor: '#000'
+    var plot = document.getElementById('plot'); 
+    var myChart = new Chart(plot, {
+        type: 'line',
+        data: {
+            labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            datasets: [{
+                label: '',
+                data: [12, 19, 3],
+                borderWidth: 1,
+                backgroundColor: '#0000',
+                borderColor: '#000'
+            }]
         },
-        yaxis: {
-            title: {
-                text: "Temperature",
-                standoff: 40
-            },
-            range: [40, 450],
-            autotick: false,
-            ticks: 'outside',
-            tick0: 0,
-            dtick: 50,
-            ticklen: 5,
-            tickwidth:  5,
-            tickcolor: '#000'
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-    };
-
-    var log = [];
-
-    Plotly.plot(plot,[{
-        y:[65,70],
-        type:'line'
-    }],layout);
-
-    socket.on('count', (arg) => {
-        console.log(arg); 
-        newVal = arg;
-        log.push(newVal);
-        console.log(log)
-        var tempDiv = document.getElementById('current-temp')
-        tempDiv.innerHTML = newVal;
-        Plotly.plot(plot, { y: [log]})
     });
+
+    var newData = [4,4,20]
+    function addData(myChart, label, data) {
+        console.log('updating data')
+        myChart.data.labels.push(label);
+        for(i=0; i < data.length; i++) {
+        myChart.data.datasets[0].data.push(data[i])
+        myChart.update();
+        }
+    }
+    addData(myChart,['11'],newData)
+    // socket.on('count', (arg) => {
+    //     console.log(arg); 
+    //     newVal = arg;
+    //     log.push(newVal);
+    //     console.log(log)
+    //     var tempDiv = document.getElementById('current-temp')
+    //     tempDiv.innerHTML = newVal;
+            
+    // });
 
 })();
