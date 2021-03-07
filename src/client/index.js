4df4c77a39555284f152ab0d94e5b100ba75a0fc
Joseph <josephplaugher@gmail.com>
2021-03-06 21:18:53 -0800

    plotly isnt working for me. branching to try chartjs

--- src/client/index.js
+++ src/client/index.js
@@ -1,22 +1,7 @@
 (function () {
     const socket = io();
-    var log = [];
-    var time = []
-      socket.on('count', (arg) => {
-        console.log(arg); 
-        newVal = arg;
-        log.push(newVal);
-        var str = log.toString();
-        var holder = document.getElementById('message');
-        holder.innerHTML = str
-      });
-
 
     plot = document.getElementById('plot');
-    var data = [{
-        x: [1, 2, 3, 3.2],
-        y: [50,100,150,200,250,300,350,400],
-        type: 'scatter'}]
     var layout = {
         xaxis: {
             title: {
@@ -43,9 +28,26 @@
             tick0: 0,
             dtick: 50,
             ticklen: 5,
-            tickwidth: 5,
+            tickwidth:  5,
             tickcolor: '#000'
         }
     };
-    Plotly.newPlot(plot, data, layout);
+
+    var log = [];
+
+    Plotly.plot(plot,[{
+        y:[65,70],
+        type:'line'
+    }],layout);
+
+    socket.on('count', (arg) => {
+        console.log(arg); 
+        newVal = arg;
+        log.push(newVal);
+        console.log(log)
+        var tempDiv = document.getElementById('current-temp')
+        tempDiv.innerHTML = newVal;
+        Plotly.plot(plot, { y: [log]})
+    });
+
 })();
--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,14 @@
 // const dotenv = require('dotenv').config()
-// const SerialPort = require('serialport');
-// const Readline = require('@serialport/parser-readline');
-// const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
+
 const path = require('path');
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+//open the serial port
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
 
 const PORT = 3004
 http.listen(PORT, () => {
@@ -31,11 +33,9 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
 	console.log('socket open')
-	// const parser = port.pipe(new Readline({ delimiter: '\n' }));// Read the port data
-	// port.on("open", () => {
-	// 	console.log('serial port open');
-	// });
-	// parser.on('data', data =>{
-	// 	socket.emit('count', data)
-	// });
+	const parser = port.pipe(new Readline({ delimiter: '\n' }));// Read the port data
+	parser.on('data', data =>{
+		data = parseInt(data.substring(0, data.length - 1));
+		socket.emit('count', data)
+	});
 });
