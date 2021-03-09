c80067c683b6a9fac6e2e0fbd22f730bbd3cae85
Joseph <josephplaugher@gmail.com>
2021-03-09 06:15:47 -0800

    the chart is looking good. still need to see if arduino data will actual go in

--- src/client/index.js
+++ src/client/index.js
@@ -1,26 +1,37 @@
-    const MG = require('metrics-graphics')
+const socket = io();
 
-        const socket = io();
+var plot = document.getElementById('plot'); 
 
-        var plot = document.getElementById('plot'); 
+var chart = new CanvasJS.Chart(plot, {
+	animationEnabled: true,
+	theme: "light2",
+	title:{ text: "Roast Profile"},
+    axisX: { intervalType: "second", interval: 30, maximum: 600},
+	data: [{ type: "line", indexLabelFontSize: 12,
+		dataPoints: [
+			{ y: 50 },
+			{ y: 70 },
+			{ y: 100 },
+			{ y: 160 },
+			{ y: 250 },
+			{ y: 300 },
+			{ y: 380 }
+		]
+	}]
+});
+chart.render();
 
-        new MG.data_graphic({
-            data: [{'seconds':1,'value':1},
-                {'seconds':2,'value':3},
-                {'seconds':3,'value':10},
-                {'seconds':4,'value':4}
-            ],
-            width: 600,
-            height: 200,
-            target: plot,
-            area: false,
-            xAccessor: 'seconds',
-            yAccessor: 'value'
-          })
+var newData = [390,400,450]
+i =0;
+for(i < 0; i < newData.length; i++) {
+    console.log('add')
+        chart.options.data[0].dataPoints.push({ y: newData[i]});
+        chart.render();
+}
 
-        socket.on('count', (newVal) => {
-            console.log(newVal); 
-            var tempDiv = document.getElementById('current-temp')
-            tempDiv.innerHTML = newVal;
-        
-        });
+
+socket.on('count', (newVal) => {
+    console.log(newVal); 
+    var tempDiv = document.getElementById('current-temp')
+    tempDiv.innerHTML = newVal;      
+});
